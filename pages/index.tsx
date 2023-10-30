43e78355136b9142c6abc56dba2b25b26f82fe55
Yusuf Özcan GÜLER <y.ozcan.guler@gmail.com>
2023-10-30 14:17:13 +0300

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,20 @@ const IndexPage = () => {
     async function fetchData() {
         let req = await fetch("api/fetchData")
         let res = await req.json()
-        setCalendarData(res)
-        setCalendarDataString(JSON.stringify(res, null, 4))
+        // setCalendarData(res)
+        // setCalendarDataString(JSON.stringify(res, null, 4))
     }
     useEffect(() => {
         fetchData()
     }, [])
     return (
         <div style={{ padding: 20 }}>
-            <textarea disabled style={{ width: "100%", resize: "none" }} rows={40}
+            <textarea style={{ width: "100%", resize: "none" }} rows={40}
                 value={calendarDataString}>
             </textarea>
             <textarea disabled style={{ width:"100%", marginTop: 20, resize: "none" }} rows={40}
-                value={calendarData.filter((item: any) => item.impact == "High").map((item: any) => Date.parse(item.date)).join(", ")}>
+                value={calendarData.map((item: any) => Date.parse(item.date)).join(", ")}>
+                {/* value={calendarData.filter((item: any) => item.impact == "High").map((item: any) => Date.parse(item.date)).join(", ")}> */}
             </textarea>
         </div>
     )
