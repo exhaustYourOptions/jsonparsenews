e659e89f93251504b46dfbdb47e7f3a1b7d4859a
Yusuf Özcan GÜLER <y.ozcan.guler@gmail.com>
2023-10-30 15:31:23 +0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,21 +7,30 @@ const IndexPage = () => {
     const [calendarDataString, setCalendarDataString] = useState("")
     const [dataToShow, setDataToShow] = useState([])
     const [filterImpact, setFilterImpact] = useState("All")
+    const [filterCurrency, setFilterCurrency] = useState("All")
+    const [currencies, setCurrencies] = useState([])
+    const [impacts, setImpacts] = useState([])
+
     async function fetchData() {
         let req = await fetch("api/fetchData")
         let res = await req.json()
-        // setCalendarData(res)
-        // setCalendarDataString(JSON.stringify(res, null, 4))
+        setCalendarData(res)
+        setCalendarDataString(JSON.stringify(res, null, 4))
     }
 
-    const filter = ()=>{
-        console.log("filter", filterImpact)
+    const filter = () => {
         try {
-            if(filterImpact == "All"){
-                setDataToShow(JSON.parse(calendarDataString).map((item: any) => Date.parse(item.date)).join(", "))
+            let filtered = []
+            let items = JSON.parse(calendarDataString)
+            if (filterImpact != "All") {
+                filtered = items.filter((item: any) => item.impact == filterImpact)
             }else{
-                setDataToShow(JSON.parse(calendarDataString).filter((item: any) => item.impact == filterImpact).map((item: any) => Date.parse(item.date)).join(", "))
+                filtered = items
             }
+            if (filterCurrency != "All") {
+                filtered = filtered.filter((item: any) => item.country == filterCurrency)
+            }
+            setDataToShow(filtered)
         } catch (error) {
             console.error(error)
         }
@@ -33,17 +42,41 @@ const IndexPage = () => {
         } catch (error) {
         }
     }, [calendarDataString])
+
+    useEffect(() => {
+        try {
+            const _currencies = ["All"]
+            const _impacts = ["All"]
+    
+            for (let i = 0; i < calendarData.length; i++) {
+                const item = calendarData[i];
+                if (_currencies.includes(item.country) == false) {
+                    _currencies.push(item.country)
+                }else{
+                    console.log(item)
+                }
+                if (_impacts.includes(item.impact)==false) {
+                    _impacts.push(item.impact)
+                }
+            }
+            console.log("_currencies", _currencies)
+            console.log("_impacts", _impacts)
+            setCurrencies(_currencies)
+            setImpacts(_impacts)
+            
+        } catch (error) {
+            
+        }
+
+    }, [calendarData])
     useEffect(() => {
         fetchData()
     }, [])
 
-    useEffect(()=>{
-        
+    useEffect(() => {
+
         filter()
-    }, [filterImpact])
-    const handleFilterChange = (e)=>{
-        setFilterImpact(e.target.value)
-    };
+    }, [filterImpact, filterCurrency])
 
     return (
         <div style={{ padding: 20 }}>
@@ -56,16 +89,33 @@ const IndexPage = () => {
                 }}
             >
             </textarea>
-            <select style={{fontSize:24}} onChange={handleFilterChange} value={filterImpact}>
-            <optgroup label="Impact ">
-              <option value="All">All</option>
-              <option value="High">High</option>
-              <option value="Medium">Medium</option>
-              <option value="Low">Low</option>
-            </optgroup>
-          </select>
-            <textarea disabled style={{ width: "100%", marginTop: 20, resize: "none" }} rows={40}
-                value={dataToShow}>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+                <div>Impact: </div>
+                <select style={{ fontSize: 24, marginLeft:10 }} onChange={(e) => {
+                    setFilterImpact(e.target.value)
+                }} value={filterImpact}>
+                    <optgroup label="Impact ">
+                        {impacts.map((item, index) => {
+                            return <option key={item} value={item}>{item}</option>
+
+                        })}
+                    </optgroup>
+                </select>
+                <div style={{marginLeft:50}}>Currency: </div>
+                <select style={{ fontSize: 24, marginLeft:10 }} onChange={(e) => {
+                    setFilterCurrency(e.target.value)
+                }} value={filterCurrency}>
+                    <optgroup label="Currency ">
+                        {currencies.map((item, index) => {
+                            return <option key={item} value={item}>{item}</option>
+
+                        })}
+                    </optgroup>
+                </select>
+
+            </div>
+            <textarea disabled style={{ width: "100%", marginTop: 20, resize: "none" }} rows={20}
+                value={dataToShow.map((item: any) => Date.parse(item.date)).join(", ")}>
                 {/* value={calendarData.filter((item: any) => item.impact == "High").map((item: any) => Date.parse(item.date)).join(", ")}> */}
             </textarea>
         </div>
